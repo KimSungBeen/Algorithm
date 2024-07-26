8085215b1ae02139196ca7a8c16452f1088b6996
김성빈 <57839497+KimSungBeen@users.noreply.github.com>
2024-07-26 19:00:16 +0900

    [level 2] Title: 전화번호 목록, Time: 361.31 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,13 +3,11 @@ import java.util.Comparator;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-       // 전화번호의 길이는 1~20 이고
-        // 배열의 길이는 1 ~ 1,000,000 이므로
-        // 효율적인 탐색을 위해 배열을 전화번호 길이의 오름차순으로 정렬
+        // 효율적인 탐색을 위해 배열을 정렬
         Arrays.sort(phone_book);
 
-        // 배열의 첫번째 원소부터 순차적으로 다른 원소들의 접두어인 경우가 있는지 탐색 하고
-        // 있다면 false 를 리턴
+        // 위에서 정렬을 했으므로 붙어있는 원소끼리만 비교하면 
+        // 한 번호가 다른 번호의 접두어인 경우가 있는지 여부를 알 수 있음 
         for (int i = 0; i < phone_book.length - 1; i++) {
             if (phone_book[i + 1].startsWith(phone_book[i])) {
                 return false;
