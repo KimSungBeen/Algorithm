c1ad3ab99adf95b20db94bb4b9ce4ea91b4e3b81
김성빈 <57839497+KimSungBeen@users.noreply.github.com>
2024-08-27 15:36:18 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 23.63 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,56 +1,49 @@
-import java.util.ArrayList;
- 
+import java.util.Stack;
+
 class Solution {
-    static ArrayList<Integer>[] graph;
-    static int min;
- 
     public int solution(int n, int[][] wires) {
-        graph = new ArrayList[n + 1];
-        min = Integer.MAX_VALUE;
- 
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
- 
+        int answer = n;
+
+        // 전체 wire가 한번씩 끊어진 경우 순회
         for (int i = 0; i < wires.length; i++) {
-            int v1 = wires[i][0];
-            int v2 = wires[i][1];
-            graph[v1].add(v2);
-            graph[v2].add(v1);
-        }
- 
-        for (int i = 0; i < wires.length; i++) {
-            int v1 = wires[i][0];
-            int v2 = wires[i][1];
- 
-            boolean[] visited = new boolean[n + 1];
- 
-            graph[v1].remove(Integer.valueOf(v2));
-            graph[v2].remove(Integer.valueOf(v1));
- 
-            int cnt = dfs(1, visited);
- 
-            int diff = Math.abs(cnt - (n - cnt));
-            min = Math.min(min, diff);
- 
-            graph[v1].add(v2);
-            graph[v2].add(v1);
-        }
- 
-        return min;
-    }
- 
-    static int dfs(int v, boolean[] visited) {
-        visited[v] = true;
-        int cnt = 1;
- 
-        for (int next : graph[v]) {
-            if (!visited[next]) {
-                cnt += dfs(next, visited);
+            int[] cutWire = wires[i]; // 끊어진 wire
+
+            // wire가 끊어진 송전탑과 연결된 송전탑을 찾아 stack에 넣을 때,
+            // 중복 방지를 위한 플래그
+            boolean[] linked = new boolean[n + 1];
+            Stack<Integer> s = new Stack<>();
+
+            // 첫 순회 송전탑 push
+            s.push(cutWire[0]);
+            linked[cutWire[0]] = true;
+
+            int cnt = 0;
+
+            while (!s.isEmpty()) {
+                int top = s.pop();
+                cnt++;
+
+                // 끊어진 wire를 제외한 모든 wire 순회하며
+                // 첫 순회 송전탑과 연결된 송전탑을 stack에 추가
+                for (int j = 0; j < wires.length; j++) {
+                    if (i == j) continue;
+
+                    if (wires[j][0] == top && !linked[wires[j][1]]) {
+                        s.push(wires[j][1]);
+                        linked[wires[j][1]] = true;
+                    } else if (wires[j][1] == top && !linked[wires[j][0]]) {
+                        s.push(wires[j][0]);
+                        linked[wires[j][0]] = true;
+                    }
+                }
             }
+
+            int group1 = cnt;
+            int group2 = Math.abs(n - cnt);
+
+            answer = Math.min(answer, Math.abs(group1 - group2));
         }
- 
-        return cnt;
+
+        return answer;
     }
 }
- 
