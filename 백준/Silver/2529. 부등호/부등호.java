9aabb53284eb744c842d2beca705973fde5c8130
김성빈 <57839497+KimSungBeen@users.noreply.github.com>
2024-08-30 14:23:07 +0900

    [Silver I] Title: 부등호, Time: 200 ms, Memory: 20232 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,49 +1,81 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    static ArrayList<String> results = new ArrayList<>();
-    static char[] signs;
-    static boolean[] used = new boolean[10];
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Solution s = new Solution();
+        s.main();
+    }
+}
 
-        int k = sc.nextInt();
-        signs = new char[k];
+class Solution {
+    /** 부등호 개수 */
+    int k;
 
-        for (int i = 0; i < k; i++) {
-            signs[i] = sc.next().charAt(0);
+    /** 부등호 배열 */
+    String[] inequalitySigns;
+
+    /** 숫자 사용 여부 0 ~ 9 */
+    boolean[] used;
+
+    /** 답이 될 수 있는 리스트 */
+    List<String> answers = new ArrayList<>();
+
+    void main() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            // 입력을 받는다
+            this.k = Integer.parseInt(br.readLine());
+            this.inequalitySigns = br.readLine().split(" ");
+            this.used = new boolean[10];
+
+            dfs(0, new StringBuilder());
+
+            Collections.sort(this.answers);
+
+            bw.write(this.answers.get(this.answers.size() - 1));
+            bw.write("\n");
+            bw.write(this.answers.get(0));
+
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        dfs("", 0, k);
-
-        Collections.sort(results);
-
-        System.out.println(results.get(results.size() - 1)); // 최대값 출력
-        System.out.println(results.get(0)); // 최소값 출력
     }
 
-    // DFS를 이용하여 모든 가능한 숫자 조합을 확인
-    static void dfs(String num, int depth, int k) {
-        if (depth == k + 1) {
-            results.add(num);
+    private void dfs(int index, StringBuilder num) {
+        // 반복 횟수가 k + 1번째라면 등식이 완성된 것이므로 답 리스트에 추가후 함수를 리턴한다.
+        if (index == this.k + 1) {
+            this.answers.add(num.toString());
             return;
         }
 
-        for (int i = 0; i <= 9; i++) {
-            if (!used[i]) {
-                if (depth == 0 || check(num.charAt(depth - 1) - '0', i, signs[depth - 1])) {
-                    used[i] = true;
-                    dfs(num + i, depth + 1, k);
-                    used[i] = false;
-                }
+        for (int i = 0; i < 10; i++) {
+            if (this.used[i]) continue;
+
+            // 첫번째 숫자는 무조건 추가,
+            // 마지막으로 추가된 숫자와 새로 추가될 숫자의 조건연산이 참이면 StringBuilder에 값 추가
+            // 재귀호출에서 돌아올 때 백트래킹 처리
+            if (index == 0 || check(num.charAt(num.length() - 1) - '0', i, this.inequalitySigns[index - 1])) {
+                num.append(i);
+                this.used[i] = true;
+                dfs(index + 1, num);
+                this.used[i] = false;
+                num.deleteCharAt(num.length() - 1); // 마지막에 추가된 숫자를 삭제
             }
         }
     }
 
-    // 부등호를 체크하여 숫자가 조건을 만족하는지 확인
-    static boolean check(int a, int b, char sign) {
-        if (sign == '<') return a < b;
-        else return a > b;
+    private boolean check(int a, int b, String sign) {
+        return sign.equals("<") ? a < b : a > b;
     }
 }
