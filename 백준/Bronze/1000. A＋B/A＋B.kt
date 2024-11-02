b5058bb50273524d85720b1f55ddba9853bba685
김성빈 <57839497+KimSungBeen@users.noreply.github.com>
2024-11-02 17:22:51 +0900

    [Bronze V] Title: A+B, Time: 116 ms, Memory: 21364 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    BufferedWriter(OutputStreamWriter(System.out)).use { bw: BufferedWriter ->
+        val result: Int = br.readLine()
+            .split(" ")
+            .map { it.toInt() }
+            .reduce { acc, i -> acc + i }
+
+        bw.write("$result")
+    }
+}
